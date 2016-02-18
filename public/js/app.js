552670e872d4fb83eb7c48411497d02f0e4676a1
aboone08 <andranique.boone@gmail.com>
2016-02-17 19:24:58 -0500

    some changes

--- public/js/app.js
+++ public/js/app.js
@@ -1,7 +1,7 @@
-  var weatherData
+  var weatherData = {};
 $(document).ready(function(){
   var baseUrl = 'https://api.forecast.io/forecast/';
-  
+  var name = "Your Name";
 
   $('#get-weather').on('click', getWeather);
 
